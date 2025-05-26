6c30e21b27d1d744105baa2c84ae7b2e9b66488f
Rajnishdimaha <rajnishkumardimaha@gmail.com>
2025-05-26 17:18:34 +0530

    raj comit

--- JenkinWebApp/src/main/java/com/nt/servlet/SeasonFinderServlet.java
+++ JenkinWebApp/src/main/java/com/nt/servlet/SeasonFinderServlet.java
@@ -4,69 +4,41 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.time.LocalDateTime;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-@SuppressWarnings("serial")
 @WebServlet("/season")
 public class SeasonFinderServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse res)
+    public void doGet(HttpServletRequest req, HttpServletResponse res)
             throws ServletException, IOException {
 
-        System.out.println("SeasonFinderServlet.doGet()");
-
-        // Set content type
+        PrintWriter pw = res.getWriter();
         res.setContentType("text/html");
 
-        // Get PrintWriter to write response
-        PrintWriter pw = res.getWriter();
+        LocalDateTime ldt = LocalDateTime.now();
+        int month = ldt.getMonthValue();
 
-        // Get current date and time
-        LocalDateTime now = LocalDateTime.now();
-        int month = now.getMonthValue();
-
-        // Determine season
         String seasonName;
-        if (month >= 3 && month <= 6) {
+        if (month >= 3 && month <= 6)
             seasonName = "Summer Season";
-        } else if (month >= 7 && month <= 10) {
+        else if (month >= 7 && month <= 10)
             seasonName = "Rainy Season";
-        } else {
+        else
             seasonName = "Winter Season";
-        }
 
-        // Generate HTML response
-        pw.println("<!DOCTYPE html>");
-        pw.println("<html>");
-        pw.println("<head>");
-        pw.println("<meta charset='UTF-8'>");
-        pw.println("<title>Season Finder</title>");
-        pw.println("<style>");
-        pw.println("body { text-align: center; font-family: Arial, sans-serif; }");
-        pw.println("h1 { color: green; }");
-        pw.println("</style>");
-        pw.println("</head>");
-        pw.println("<body>");
-        pw.printf("<h1>%s</h1>%n", seasonName);
-        pw.println("<br>");
-        pw.println("<a href='index.jsp'>");
-        pw.println("Home <img src='Images/home.png' width='50' alt='Home'>");
-        pw.println("</a>");
-        pw.println("</body>");
-        pw.println("</html>");
-
+        pw.println("<h1 style='color:green; text-align:center'>" + seasonName + "</h1>");
+        pw.println("<a href='index.jsp'>Home <img src='Images/home.png' width='50'></a>");
         pw.close();
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse res)
+    public void doPost(HttpServletRequest req, HttpServletResponse res)
             throws ServletException, IOException {
-        System.out.println("SeasonFinderServlet.doPost()");
         doGet(req, res);
     }
 }
--- JenkinWebApp/src/main/webapp/index.jsp
+++ JenkinWebApp/src/main/webapp/index.jsp
@@ -1,27 +1,18 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>Season Display</title>
     <style>
-        h1 {
-            color: red;
-            text-align: center;
-        }
-        .link-container {
-            text-align: center;
-            margin-top: 40px;
-        }
+        h1 { color: red; text-align: center; }
+        .link-container { text-align: center; margin-top: 40px; }
     </style>
 </head>
 <body>
-
     <h1>HTML to Servlet Communication</h1>
-
     <div class="link-container">
         <a href="season">Show Season Name</a>
     </div>
-
 </body>
 </html>
